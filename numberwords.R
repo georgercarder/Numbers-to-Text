e4c6cde00b80efb6aa0db041985a27dc483d02f5
George Carder <georgercarder@gmail.com>
2017-09-10 05:05:29 -0700

    Update numberwords.R

--- numberwords.R
+++ numberwords.R
@@ -1,7 +1,5 @@
 #/bin/Rscript
 
-#print("given number n")
-####first find degree of polynomial "in 10" and thousands millions etc
 l1=1
 l2=10*l1
 p=0
@@ -9,9 +7,6 @@ t=0
 tag=1
 while(tag>0){
     if(n>=l1&n<l2){
-        #print("p and t is")
-        #print(p)
-        #print(t)
         tag=0
     }else{
         l1=10*l1
@@ -24,14 +19,14 @@ while(tag>0){
     }
     }
 }
-##########
+
 ones<-c("one","two","three","four","five","six","seven","eight","nine")
 ## exception for teens
 tens<-c("twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety")
 teens<-c("ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eightteen","nineteen")
 hundreds<-c("one hundred","two hundred","three hundred","four hundred","five hundred","six hundred","seven hundred","eight hundred","nine hundred")
 thousands<-c("thousand","million","billion","trillion","quadrillion","quintillion")
-##pos is position 
+#pos is position
 pos=p
 tos=t
 tt=0
@@ -41,33 +36,23 @@ word<-""
 
 while(pos>0){ 
 x=floor(m/(10^(pos)))
-#print("x is")
-#print(x)
     if(pos%%3==0&tt!=1){
         if(x==0){
         word<-stringi::stri_join(word,"and")
         }else{
-        #print(ones[x])
         word<-stringi::stri_join(word,ones[x]) 
-        #print("word00 tt")
-        #print(word)
-        #print(tt)
         }  
     }else if(pos%%3==1){
         if(x==0){
-            #word<-stringi::stri_join(word,"and")
+            
         }else if(x==1){
             tt=1
             y=floor((m-10^pos)/(10^(pos-1)))
-            #print(teens[y+1])
             word<-stringi::stri_join(word,teens[y+1])
-            #print("words in ")
-            #print(word)
             m=m-y*10^(pos-1)
-            #print("m in")
-            #print(m)
+            
         }else{
-            #print(tens[x-1])    
+            
             word<-stringi::stri_join(word,tens[x-1])
             
         }   
@@ -77,14 +62,12 @@ x=floor(m/(10^(pos)))
             word<-stringi::stri_join(word,"and")
             
         }else{        
-            #print(hundreds[x])
-            #print("and")
+            
             
             word<-stringi::stri_join(word, hundreds[x]," ","and")
         }
     }
     if(pos%%3==0||(pos%%3==1&x==1)&pos!=1){
-        #print(thousands[tos])
         word<-stringi::stri_join(word," ",thousands[tos])
         tos=tos-1 
         tt=0           
@@ -92,28 +75,16 @@ x=floor(m/(10^(pos)))
 
 
 word<-stringi::stri_join(word," ")
-#print("wordss")
-#print(word)
 
-#print("pos is")
-#print(pos)
-#print("word is")
-#print(word)
 
 m=m-x*10^pos
 
-#print("m is")
-#print(m)
 if(pos%%3==1&(x==1)){
 pos=pos-1
-#print("posif is")
-#print(pos)
 }
 
 
 pos=pos-1
-#print("posend is")
-#print(pos)
 }
 
 if(pos==0||-1){
@@ -121,7 +92,7 @@ if(pos==0||-1){
      if(x==0){
      
      }else{
-     #print(ones[x])
+     
      word<-stringi::stri_join(word,ones[x])
      }
 }
