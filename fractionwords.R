56846efd68ed74acca41c3086bc07ee1ce42ba95
George Carder <georgercarder@gmail.com>
2017-09-15 16:34:20 -0700

    Add files via upload

---
+++ fractionwords.R
@@ -0,0 +1,35 @@
+#!/bin/Rscript
+
+A<-sample(0:100000000,10,replace=FALSE,prob=NULL)
+B<-sample(1:100000000,10,replace=FALSE,prob=NULL)
+i=1
+while(i<=10){
+
+f<-stringi::stri_join(as.character(A[i]),"/",as.character(B[i]))
+
+#given f="a/b"
+
+# parse F to get a and b separately
+#find out if we reduce fractions. assuming not we continue
+a<-as.double(gsub("/.*","",f))
+b<-as.double(gsub(".*/","",f))
+
+
+n<-a
+source("numberwords.R")
+#print("worda is")
+#print(worda)
+
+
+n<-b
+source("thsnumberwords.R")
+#print("wordb is")
+#print(wordb)
+
+wordfrac<-stringi::stri_join(worda," ",wordb)
+print(f)
+#print("wordfrac is")
+print(wordfrac)
+
+i=i+1
+}
