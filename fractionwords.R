3d4c749932a425c17fe43b61d466953aa4398cff
George Carder <georgercarder@gmail.com>
2017-09-15 16:38:38 -0700

    Update fractionwords.R

--- fractionwords.R
+++ fractionwords.R
@@ -17,18 +17,13 @@ b<-as.double(gsub(".*/","",f))
 
 n<-a
 source("numberwords.R")
-#print("worda is")
-#print(worda)
 
 
 n<-b
 source("thsnumberwords.R")
-#print("wordb is")
-#print(wordb)
 
 wordfrac<-stringi::stri_join(worda," ",wordb)
 print(f)
-#print("wordfrac is")
 print(wordfrac)
 
 i=i+1
